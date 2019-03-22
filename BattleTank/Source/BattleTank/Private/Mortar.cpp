656b0ddf1ad17bfff275cd7704136a7748145f48
cafragachan <cafragachan@yahoo.com>
2019-03-22 18:42:08 +0000

    Mortar Blueprint

---
+++ BattleTank/Source/BattleTank/Private/Mortar.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Mortar.h"
+
+
+// Sets default values
+AMortar::AMortar()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void AMortar::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+// Called to bind functionality to input
+void AMortar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
